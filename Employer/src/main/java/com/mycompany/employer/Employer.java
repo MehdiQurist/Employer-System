ca78ed13f3b2ff61b9bbecfead7ca9ccf68a7318
Mehdi Qurbanov <105870612+MehdiQurist@users.noreply.github.com>
2026-03-02 07:50:26 +0200

    Add files via upload

---
+++ Employer/src/main/java/com/mycompany/employer/Employer.java
@@ -0,0 +1,19 @@
+
+package com.mycompany.employer;
+
+public class Employer {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+  
+
+        
+        
+        
+    }
+    
+   
+        
+    }
+
---
+++ Employer/src/main/java/com/mycompany/employer/Point.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.employer;
+
+import com.sun.tools.javac.Main;
+import java.io.IOException;
+
+public class Point {
+    
+    public static void main(String[] args) {
+        
+        int x= 0 ;
+        int y = 0 ;
+       
+     }
+}
+     
